a5d9ada694b3b615e05da0e4ee73525691c8818c
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2026-09-20 12:28:09 -0300

    implementações de teste da classe servicoCliente
    Implementando testes com unittest da classe servicoClientes. Com esses testes consigo verificar o funcionamento das regras de negocios e da chamada do repositorio para o banco de dados, assim como todo o caminho do sistema desde a regra de negocios de servicoCliente, passando pela chamada do banco de dados em repositorioClientes, sem necessariamente alterar o banco de dados para não haver posteriomente confusão de informações desnecessarias.

--- testes/TesteServicos/teste_mock_cliente.py
+++ testes/TesteServicos/teste_mock_cliente.py
@@ -3,7 +3,6 @@ from unittest.mock import Mock
 from servicos.cliente_servico import ClienteServico
 from models.cliente import Cliente
 
-
 class TestClienteServico(unittest.TestCase):
     def setUp(self):
         self.servico = ClienteServico()
@@ -12,12 +11,25 @@ class TestClienteServico(unittest.TestCase):
 
     def test_cadastrar_cliente_sucesso(self):
         cliente = Cliente("João", "098.050.987.66",81998599798, "aaa@gmail.com")
-        self.mock_repositorio.cadastrar.return_value = cliente
+        self.mock_repositorio.cadastrar.return_value = True
         resultado = self.servico.cadastrar(cliente)
         self.mock_repositorio.cadastrar.assert_called_once_with(cliente)
         self.assertEqual(resultado,'Cliente cadastrado com sucesso!')
+
     def test_cadastrar_cliente_nomeCpf_em_branco(self):
         cliente = Cliente("","",819958756,"aaa@gmail.com")
+        resultado = self.servico.cadastrar(cliente)
+        self.mock_repositorio.cadastrar.assert_not_called()
+        self.assertEqual(resultado, "Por Favor, insira um valor valido nos campos Nome e Cpf!")
+
+    def test_cadastrar_cliente_nome_em_branco(self):
+        cliente = Cliente("", "095.055.123.12", 819958756, "aaa@gmail.com")
+        resultado = self.servico.cadastrar(cliente)
+        self.mock_repositorio.cadastrar.assert_not_called()
+        self.assertEqual(resultado, "Por Favor, insira um valor valido nos campos Nome e Cpf!")
+
+    def test_cadastrar_cliente_cpf_em_branco(self):
+        cliente = Cliente("Benjamin", "",8178956321,"ben10@hotmail.com")
         resultado = self.servico.cadastrar(cliente)
         self.mock_repositorio.cadastrar.assert_not_called()
         self.assertEqual(resultado, "Por Favor, insira um valor valido nos campos Nome e Cpf!")
@@ -29,4 +41,48 @@ class TestClienteServico(unittest.TestCase):
         self.mock_repositorio.cadastrar.assert_called_once_with(cliente)
         self.assertEqual(resultado,"Cliente não cadastrado! (Resposta negativa do Banco de dados)")
 
+    def test_listar_todos_sucesso(self):
+        clientes = ["Cliente1", "Cliente2", "Cliente3"]
+        self.mock_repositorio.listar_todos.return_value = clientes
+        resultado = self.servico.listar_todos()
+        self.mock_repositorio.listar_todos.assert_called_once_with ()
+        self.assertEqual(resultado, clientes)
+
+    def test_lista_todos_lista_vazia(self):
+        self.mock_repositorio.listar_todos.return_value = []
+        resultado = self.servico.listar_todos()
+        self.mock_repositorio.listar_todos.assert_called_once_with()
+        self.assertEqual(resultado, "Não existe clientes cadastrados!!")
+
+    def test_buscar_por_id_existente(self):
+        cliente = Cliente("Maria", "011.254.699-44")
+        self.mock_repositorio.buscar_por_id.return_value = cliente
+        resultado = self.servico.buscar_por_id(2)
+        self.mock_repositorio.buscar_por_id.assert_called_once_with(2)
+        self.assertEqual(resultado,f"Cliente: {cliente}")
+
+    def test_buscar_por_id_inexistente(self):
+        self.mock_repositorio.buscar_por_id.return_value = []
+        resultado = self.servico.buscar_por_id(2)
+        self.mock_repositorio.buscar_por_id.assert_called_once_with(2)
+        self.assertEqual(resultado, "Cliente inexistente!")
+
+    def test_buscar_por_nome_inexistente(self):
+        self.mock_repositorio.buscar_por_nome.return_value=[]
+        resultado = self.servico.buscar_por_nome("Jefferson")
+        self.mock_repositorio.buscar_por_nome.assert_called_once_with("Jefferson")
+        self.assertEqual(resultado, "Não foram encontrados registros com esse nome")
+
+    def test_buscar_por_nome_existente(self):
+        cliente = Cliente("Jefferson", "254.568.555-98",8195785455, "ccc@hotmail.com")
+        self.mock_repositorio.buscar_por_nome.return_value = cliente
+        resultado = self.servico.buscar_por_nome("Jefferson")
+        self.mock_repositorio.buscar_por_nome.assert_called_once_with("Jefferson")
+        self.assertEqual(resultado,cliente)
+
+    def test_buscar_por_nome_nome_vazio(self):
+        cliente = Cliente('','098.145.689-66',34447985, 'www@email.com')
+        resultado = self.servico.buscar_por_nome("")
+        self.mock_repositorio.buscar_por_nome.assert_not_called()
+        self.assertEqual(resultado,"Insira um nome para iniciar a pesquisa!")
 

ade8522538e5d222ace5c54e2f4969fbd095b508
JeffersonSimplicio1 <jefferson.a.simplicio@gmail.com>
2026-09-24 21:32:43 -0300

    Testes da classe funcionarios finalizado

--- testes/TesteServicos/teste_mock_funcionario.py
+++ testes/TesteServicos/teste_mock_funcionario.py
@@ -96,7 +96,7 @@ class TestFuncionarioServico(unittest.TestCase):
     def test_atualizar_funcionarios_sucesso(self):
         funcionario = Funcionario("Jefferson", "Gerente", 5000)
         self.mock_repositorio.buscar_por_id.return_value = 3
-        self.mock_repositorio.atualziar.return_value = funcionario
+        self.mock_repositorio.atualziar.return_value = True
         resultado = self.servico.atualizar(3, funcionario)
         self.mock_repositorio.buscar_por_id.assert_called_once_with(3)
         self.mock_repositorio.atualizar.assert_called_once_with(3, funcionario)
@@ -136,6 +136,65 @@ class TestFuncionarioServico(unittest.TestCase):
         self.mock_repositorio.atualizar.assert_called_once_with(3,funcionario)
         self.assertEqual(resultado, "Impossível atualizar no momento!\n"
                                "- Erro no servidor!")
+
+    def test_excluir_por_id_Sucesso(self):
+        self.mock_repositorio.buscar_por_id.return_value = 3
+        self.mock_repositorio.excluir_por_id.return_value = True
+        resultado = self.servico.excluir_por_id(3)
+        self.mock_repositorio.buscar_por_id.assert_called_once_with(3)
+        self.mock_repositorio.excluir_por_id.assert_called_once_with(3)
+        self.assertEqual(resultado, "Ação bem sucedida!")
+
+    def test_excluir_por_id_inexistente(self):
+        self.mock_repositorio.buscar_por_id.return_value = []
+        self.mock_repositorio.excluir_por_id.return_value = 3
+        resultado = self.servico.excluir_por_id(3)
+        self.mock_repositorio.buscar_por_id.assert_called_once_with(3)
+        self.mock_repositorio.excluir_por_id.assert_not_called()
+        self.assertEqual(resultado,"Não existe funcionário com a matricula inserida!")
+
+    def test_excluir_por_id_erro_no_repositorio(self):
+        self.mock_repositorio.buscar_por_id.return_value= 3
+        self.mock_repositorio.excluir_por_id.return_value = False
+        resultado = self.servico.excluir_por_id(3)
+        self.mock_repositorio.buscar_por_id.assert_called_once_with(3)
+        self.mock_repositorio.excluir_por_id.assert_called_once_with(3)
+        self.assertEqual(resultado,"Funcionário não excluído(a)!\n"
+                       "Erro no servidor!")
+
+    def test_excluir_por_nome_sucesso(self):
+        lista = ["Nome1","Nome2","Nome3"]
+        self.mock_repositorio.buscar_por_nome.return_value = lista
+        self.mock_repositorio.excluir_por_nome.return_value = True
+        resultado = self.servico.excluir_por_nome("Nome")
+        self.mock_repositorio.buscar_por_nome.assert_called_once_with("Nome")
+        self.mock_repositorio.excluir_por_nome.assert_called_once_with("Nome")
+        self.assertEqual(resultado, "Ação bem sucedida!")
+
+    def test_excluir_por_nome_inexistente(self):
+        self.mock_repositorio.buscar_por_nome.return_value = []
+        self.mock_repositorio.excluir_por_nome.return_value = False
+        resultado = self.servico.excluir_por_nome("Nome")
+        self.mock_repositorio.buscar_por_nome.assert_called_once_with("Nome")
+        self.mock_repositorio.excluir_por_nome.assert_not_called()
+        self.assertEqual(resultado, "Não há funcionario(s) cadastrado com esse nome")
+
+    def test_excluir_por_nome_nome_em_branco(self):
+        self.mock_repositorio.buscar_por_nome.return_value = False
+        self.mock_repositorio.excluir_por_nome.return_value = False
+        resultado = self.servico.excluir_por_nome("  ")
+        self.mock_repositorio.buscar_por_nome.assert_not_called()
+        self.mock_repositorio.excluir_por_nome.assert_not_called()
+        self.assertEqual(resultado, "Insira um valor no campo nome!")
+
+    def test_excluir_por_nome_erro_repositorio(self):
+        self.mock_repositorio.excluir_por_nome.return_value = False
+        resultado = self.servico.excluir_por_nome("Nome")
+        self.mock_repositorio.buscar_por_nome.assert_called_once_with("Nome")
+        self.mock_repositorio.excluir_por_nome.assert_called_once_with("Nome")
+        self.assertEqual(resultado, "Impossivel excluir funcionario(s)\n"
+                          "Erro no servidor!")
+
 
 
 if __name__ == "__main__":
